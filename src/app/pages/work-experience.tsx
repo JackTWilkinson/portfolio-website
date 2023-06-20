1c9228c55d75121f021c950644ec94fb426db540
Jack <wilkinsonjthomas@gmail.com>
2023-06-20 17:48:19 -0600

    Fixed issue with padding/added daisyui comp

--- src/app/pages/work-experience.tsx
+++ src/app/pages/work-experience.tsx
@@ -3,6 +3,20 @@ const WorkExperience = () => {
     return(
         <>  
           <h1> Software Engineer @ Principal Financial</h1>
+          <div className="collapse w-96 border rounded-box border-base-300 collapse-arrow">
+            <input type="checkbox"/> 
+            <div className="collapse-title text-xl font-medium">
+              Software Engineer @ Principal Financial
+            </div> 
+            <div className="collapse-content"> 
+              <p></p>
+                I have been working at principal for a year now and use the following languages: 
+                <span className="badge badge-outline">Salesforce</span>
+                <span className="badge badge-outline">Java</span>
+                <span className="badge badge-outline">React</span>
+              </p>
+            </div>
+          </div>
         </>
     )
   }
