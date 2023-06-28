8cf025b0b14bec8b376ede7eb7064073cb10c58a
Jack <wilkinsonjthomas@gmail.com>
2023-06-27 23:44:04 -0600

    Working on svg compatibility

--- src/app/pages/title.tsx
+++ src/app/pages/title.tsx
@@ -1,4 +1,5 @@
 export type TitleProps = {};
+import Image from 'next/image';
 
 const Title: React.FunctionComponent<TitleProps> = () => {
 
@@ -7,10 +8,24 @@ const Title: React.FunctionComponent<TitleProps> = () => {
             <div className="flex items-center justify-center">
                 <p className="text-5xl">Jack Wilkinson</p>
                 <div>
-                    <a>Github</a>
-                    <a>Resume</a>
+                    {/* <a href="https://github.com/JackTWilkinson" target="_blank"> */}
+                        {/* <Image 
+                            src='../../../public/github.svg' 
+                            width={100} 
+                            height={100} 
+                            alt='Link to my Github profile'
+                        /> */}
+                    {/* </a> */}
+                    {/* <a href="../../../public/resume.pdf" download>
+                        Resume
+                        <Image 
+                            src='../../../public/document.svg'
+                            width={100}
+                            height={100}
+                            alt='A download link for my current Resume'
+                        />
+                    </a> */}
                 </div>
-                <button>Down Arrow</button>
             </div>
         </>
     )
